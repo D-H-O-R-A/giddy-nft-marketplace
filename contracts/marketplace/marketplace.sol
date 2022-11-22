d535a56497c93fdf0cbc810f7cac91f9bf8d0f06
D-H-O-R-A <diegoantunes2301@gmail.com>
2022-11-22 01:35:53 -0300

    contract

--- contracts/marketplace/marketplace.sol
+++ contracts/marketplace/marketplace.sol
@@ -11,7 +11,7 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-vsc
+
 
 contract AdminConsole {
 
